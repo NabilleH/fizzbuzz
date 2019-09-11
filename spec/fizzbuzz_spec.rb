1c6a374657426448c14c4aa816d575a988eafb5b
Nabille <nabilleh@outlook.com>
2019-09-11 10:55:08 +0000

    Add evaluate fizzbuzz correctly. Add further test cases

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,7 +8,20 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(4)).to eq 4
   end
   
+  it 'returns "11" when passed 11' do
+    expect(fizzbuzz(11)).to eq 11
+  end
+  
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'  
   end
+  
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  
+  it 'returns "fizzbuzz" when passed 30' do
+    expect(fizzbuzz(30)).to eq 'fizzbuzz'
+  end
+  
 end
